f33d137e1efd55b59c163cf39b78aa50fc9c5270
Andrew Luchuk <andrew.luchuk@outlook.com>
2020-01-21 09:44:38 -0500

    adds basic serializer for user

--- roomy-backend/app/services/user_serializer.rb
+++ roomy-backend/app/services/user_serializer.rb
@@ -3,6 +3,10 @@ require 'json'
 class UserSerializer
 
     def self.serialize_user(user, avatar_url)
-        {username: user.username, name: user.name, type: user.type, avatar: avatar_url}.to_json
+        {id: user.id, username: user.username, name: user.name, type: user.type, avatar: avatar_url}.to_json
+    end
+
+    def self.serialize_basic_user_info(user, avatar_url)
+        {id: user.id, name: user.name, avatar: avatar_url}.to_json
     end
 end
