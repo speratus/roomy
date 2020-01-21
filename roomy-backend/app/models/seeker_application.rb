3c17fb256689015e3f1d441e6d7f6f56ba2ca00d
Andrew Luchuk <andrew.luchuk@outlook.com>
2020-01-21 15:14:42 -0500

    adds validations for SeekerApplication

--- roomy-backend/app/models/seeker_application.rb
+++ roomy-backend/app/models/seeker_application.rb
@@ -1,4 +1,7 @@
 class SeekerApplication < ApplicationRecord
   belongs_to :user
   belongs_to :listing
+
+  validates :message, :status, presence: true
+  validates :status, inclusion: {in: %w(applied rejected accepted closed)}
 end
