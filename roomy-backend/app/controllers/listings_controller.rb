4c12a9a22fd1b9387b4a999312267077dbf58a2f
Andrew Luchuk <andrew.luchuk@outlook.com>
2020-01-21 10:55:09 -0500

    adds basic show action for listings

---
+++ roomy-backend/app/controllers/listings_controller.rb
@@ -0,0 +1,9 @@
+class ListingsController < ApplicationController
+
+    def show
+        listing = Listing.find_by(id: params[:id])
+        image_url = listing.image.attached? ? url_for(listing.image) : ""
+        render json: ListingSerializer.serialize_listing(listing, url)
+    end
+
+end
