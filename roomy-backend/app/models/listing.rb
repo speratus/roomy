56784281f8cbab51275913b72718e76fb9c23e71
Andrew Luchuk <andrew.luchuk@outlook.com>
2020-01-21 09:11:34 -0500

    adds associations to listings

--- roomy-backend/app/models/listing.rb
+++ roomy-backend/app/models/listing.rb
@@ -1,3 +1,7 @@
 class Listing < ApplicationRecord
   belongs_to :user
+  has_many :preferred_characteristics
+  has_many :characteristics, through: :preferred_characteristics
+
+  has_one_attached :image
 end
