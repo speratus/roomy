3112af29cdc62e4fa73cfa2592a9318e43b71b05
Andrew Luchuk <andrew.luchuk@outlook.com>
2020-01-21 11:05:50 -0500

    adds routes for listings and corrects controller errors

--- roomy-backend/app/controllers/listings_controller.rb
+++ roomy-backend/app/controllers/listings_controller.rb
@@ -3,7 +3,7 @@ class ListingsController < ApplicationController
     def show
         listing = Listing.find_by(id: params[:id])
         image_url = listing.image.attached? ? url_for(listing.image) : ""
-        render json: ListingSerializer.serialize_listing(listing, url)
+        render json: ListingSerializer.serialize_listing(listing, image_url)
     end
 
 end
--- roomy-backend/app/services/listing_serializer.rb
+++ roomy-backend/app/services/listing_serializer.rb
@@ -2,6 +2,13 @@ require 'json'
 
 class ListingSerializer
     def self.serialize_listing(listing, image_url)
+        
+        if listing.user.avatar.attached?
+            user_avatar_url = Rails.application.routes.url_helpers.rails_blob_path(listing.user.avatar, disposition: 'attachment')
+        else
+            user_avatar_url = ''
+        end
+
         {
             title: listing.title, 
             description: listing.description,
@@ -10,7 +17,7 @@ class ListingSerializer
             status: listing.status,
             targetNumberOfRoommates: listing.target_roommate_number,
             image: image_url,
-            user: UserSerializer.serialize_basic_user_info(listing.user, url_for(listing.user))
+            user: UserSerializer.serialize_basic_user_info(listing.user, user_avatar_url),
             updatedAt: listing.updated_at,
             preferredCharacteristics: listing.characteristics.map {|c| c.description }
         }
