3440121cff59c1383990f416d61eef4c74eaa71c
Andrew Luchuk <andrew.luchuk@outlook.com>
2020-01-21 11:31:11 -0500

    adds basic characteristic actions

--- roomy-backend/app/controllers/characteristics_controller.rb
+++ roomy-backend/app/controllers/characteristics_controller.rb
@@ -1,2 +1,33 @@
 class CharacteristicsController < ApplicationController
+
+    def create
+        characteristic = Characteristic.new(characteristic_params)
+
+        if characteristic.save
+            render json: characteristic
+        else
+            render json: {message: 'Invalid characteristic attributes'}
+        end
+    end
+
+    def index
+        characteristics = Characteristic.all
+        render characteristics
+    end
+
+    def update
+        characteristic = Characteristic.find_by(id: params[:id])
+        if characteristic.update(characteristic_params)
+            render json: characteristic
+        else
+            render json: {message: 'Invalid characteristic attributes'}
+        end
+    end
+
+    private
+
+    def characteristic_params
+        params.require(:characteristic).permit(:description)
+    end
+
 end
