2d7dc0a9c989dabd2a91089799aab09010534984
Andrew Luchuk <andrew.luchuk@outlook.com>
2020-01-21 11:24:19 -0500

    adds characteristics controller

--- roomy-backend/config/routes.rb
+++ roomy-backend/config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :users, only: [:show, :create, :delete, :update]
   resources :listings, only: [:show, :index, :update, :delete, :create]
+  resources :characteristics, except: [:edit, :new]
 end
