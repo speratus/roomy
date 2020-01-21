ccce01bfea12c72108c16a79a77c27f371ae1bf8
Andrew Luchuk <andrew.luchuk@outlook.com>
2020-01-21 11:18:56 -0500

    adds index action

--- roomy-backend/app/controllers/listings_controller.rb
+++ roomy-backend/app/controllers/listings_controller.rb
@@ -1,5 +1,10 @@
 class ListingsController < ApplicationController
 
+    def index
+        listings = Listing.all
+        render json: ListingSerializer.serialize_all(listings)
+    end
+
     def show
         listing = Listing.find_by(id: params[:id])
         image_url = listing.image.attached? ? url_for(listing.image) : ""
--- roomy-backend/app/services/listing_serializer.rb
+++ roomy-backend/app/services/listing_serializer.rb
@@ -1,6 +1,15 @@
 require 'json'
 
 class ListingSerializer
+
+    def self.get_image_url_for(listing)
+        if listing.user.avatar.attached?
+            Rails.application.routes.url_helpers.rails_blob_path(listing.user.avatar, disposition: 'attachment')
+        else
+            ''
+        end
+    end
+
     def self.serialize_listing(listing, image_url)
         
         if listing.user.avatar.attached?
@@ -20,6 +29,12 @@ class ListingSerializer
             user: UserSerializer.serialize_basic_user_info(listing.user, user_avatar_url),
             updatedAt: listing.updated_at,
             preferredCharacteristics: listing.characteristics.map {|c| c.description }
+        }
+    end
+
+    def self.serialize_all(listings)
+        listings.map { |listing|
+            ListingSerializer.serialize_listing(listing, ListingSerializer.get_image_url_for(listing))
         }
     end
 end
