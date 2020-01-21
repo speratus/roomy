012e999627e8f9b93680c4e3cb0591ef26820455
Andrew Luchuk <andrew.luchuk@outlook.com>
2020-01-21 10:38:53 -0500

    adds seed data

--- roomy-backend/db/seeds.rb
+++ roomy-backend/db/seeds.rb
@@ -5,3 +5,25 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+require 'Faker'
+
+User.delete_all
+
+5.times do
+    User.create(name: Faker::Name.name, username: Faker::Internet.email, type: 'RoomHost')
+end
+
+rand = Random.new
+
+5.times do
+    Listing.create(
+        title: 'Just your average place',
+        description: Faker::Hipster.paragraph(sentence_count: 3),
+        address: Faker::Address.full_address,
+        monthly_rent: rand.rand(2000),
+        status: 'open',
+        target_roommate_number: rand.rand(6),
+        user_id: User.all.sample.id
+    )
+end
