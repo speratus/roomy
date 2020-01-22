e9b55fbe9563dc8007ba95e5595f7ff3a9887fa4
Andrew Luchuk <andrew.luchuk@outlook.com>
2020-01-22 11:27:03 -0500

    adds basic welcome page

--- roomy-frontend/js/base.js
+++ roomy-frontend/js/base.js
@@ -4,10 +4,20 @@ class BasePage {
         this.navbar = null
     }
 
+    static clearElements(node) {
+        if (node !== null & node !== undefined) {
+            while (node.firstChild) {
+                node.firstChild.remove()
+            }
+        }
+    }
+
     showMain(e) {
         console.log(e)
         console.log("You're trying to show the base page")
         this.main.appendChild(this.navbar)
+
+        this.renderWelcomeView()
     }
 
     renderIcon(iconName, iconModifiers) {
@@ -21,6 +31,59 @@ class BasePage {
 
         return span
     }
+
+    renderFullPageBanner(mainText, contents) {
+        BasePage.clearElements(this.main.querySelector('#main-content'))
+        const main = document.createElement('main')
+        main.id = 'main-content'
+
+        const bannerSection = document.createElement('section')
+        bannerSection.className = 'hero is-primary is-fullheight'
+
+        const bannerBody = document.createElement('hero-body')
+        bannerBody.className = 'hero-body'
+
+        const centerer = document.createElement('div')
+        centerer.className = 'container has-text-centered'
+
+        const title = document.createElement('h1')
+        title.className = 'title'
+        title.textContent = mainText
+
+        centerer.appendChild(title)
+        centerer.appendChild(contents) //used to be centerer.apc(buttonLevel)
+        bannerBody.appendChild(centerer)
+        bannerSection.appendChild(bannerBody)
+        main.appendChild(bannerSection)
+
+        this.main.appendChild(main)
+
+    }
+
+    renderWelcomeView() {
+        const buttonLevel = document.createElement('div')
+        buttonLevel.className = 'level'
+
+        const buttonArea = document.createElement('div')
+        buttonArea.className = 'buttons are-medium level-item'
+
+        const loginButton = document.createElement('button')
+        loginButton.className = 'button is-link'
+        loginButton.textContent = 'Login'
+
+        const signupButton = document.createElement('button')
+        signupButton.className = 'button is-link'
+        signupButton.textContent = 'Signup'
+
+        buttonArea.appendChild(loginButton)
+        buttonArea.appendChild(signupButton)
+
+        buttonLevel.appendChild(buttonArea)
+
+        this.renderFullPageBanner('Welcome to Roomy', buttonLevel)
+    }
+
+    
 }
 
 const basePage = new BasePage()
